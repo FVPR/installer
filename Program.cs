e403a82e53755ea2157dfff59d6cf2585401783e
Felix Kaiser <felixkaiser2000@protonmail.com>
2023-02-26 03:28:31 +0100

    Changed version to 1.2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Runtime.InteropServices;
 using Newtonsoft.Json.Linq;
 
-const string Version = "1.1";
+const string Version = "1.2";
 
 var originalColor = Console.ForegroundColor;
 
